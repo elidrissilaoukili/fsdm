a86575549e5954395bfcbbd238dad8764b42f7d7
elidrissilaoukili <elidrissilaoukili@gmail.com>
2024-03-28 00:24:02 +0100

    fine

--- prog.reseau/src/reseau/verification_ad.java
+++ prog.reseau/src/reseau/verification_ad.java
@@ -3,7 +3,8 @@ package reseau;
 import java.net.InetAddress;
 
 public class verification_ad {
-     static boolean estCorrecte(String adr) {
+     @SuppressWarnings("removal")
+    static boolean estCorrecte(String adr) {
         String[] parties = adr.split("\\.");
         for (int i = 0; i < 4; i++) {
             if(new Integer (parties[i])<0 || new Integer (parties[i])>255) return false;
