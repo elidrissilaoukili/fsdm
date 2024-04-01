3759a7efac18215182ce829ae49494cfbbcc8890
elidrissilaoukili <elidrissilaoukili@gmail.com>
2024-04-01 17:19:48 +0200

    cookies

--- web/Sessions&cookies/Cookies/Exemple3/form.php
+++ web/Sessions&cookies/Cookies/Exemple3/form.php
@@ -1,6 +1,6 @@
 <h1 align="center">Cookies: Autentification</h1>
-<form name = "loginForm" action = "verifier.php" method = "post" >
-<pre>
+<form name="loginForm" action="verifier.php" method="post">
+    <pre>
 Login    <input name ="login" type = "text" /> (indication= moi)
 
 Password <input name ="pass" type = "password" /> (indication= douz)
