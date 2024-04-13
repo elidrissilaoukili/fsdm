e985063529bb83839432cbead0ee3c9f5017ba6e
elidrissi <elidrissilaoukili@gmail.com>
2024-04-13 16:11:45 +0000

    edit no change

--- plsql/triggers/t03.sql
+++ plsql/triggers/t03.sql
@@ -3,28 +3,22 @@ CREATE SEQUENCE num_etudiant_seq START WITH 10 INCREMENT BY 1 NOCACHE;
 CREATE
 OR REPLACE TRIGGER auto_increment_num_etudiant BEFORE
 INSERT
-    ON Etudiant FOR EACH ROW BEGIN :NEW.NUM_ETUDIANT := num_etudiant_seq.NEXTVAL;
+    ON Etudiant FOR EACH ROW 
 
+BEGIN 
+    :NEW.NUM_ETUDIANT := num_etudiant_seq.NEXTVAL;
 END;
 /
 
 
-
 --  After running the trigger
-INSERT INTO
-    ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
-VALUES
-    ('Mohammed', 'elidrissi', 'SMI', 22);
+INSERT INTO ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
+VALUES ('Mohammed', 'elidrissi', 'SMI', 22);
 
-INSERT INTO
-    ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
-VALUES
-    ('Mohammed', 'laoukili', 'SOME', 23);
+INSERT INTO ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
+VALUES ('Mohammed', 'laoukili', 'SOME', 23);
 
-INSERT INTO
-    ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
-VALUES
-    ('Mohammed', 'something', 'THING', 24);
-
+INSERT INTO ETUDIANT (NOM_ETUDIANT, PRENOM_ETUDIANT, Filiere, AGE)
+VALUES ('Mohammed', 'something', 'THING', 24);
 
 SELECT * FROM Etudiant;
